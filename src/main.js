64d418f71cf1ae17abffdbb851d275eb06e1848f
Mark-Jing <1320114246@qq.com>
2022-01-17 23:29:05 +0800

    newly change

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import echarts from 'echarts'
+import { Vue } from 'vue-demi'
 
 createApp(App).use(router).use(ElementPlus).mount('#app')
+Vue.prototype.$echarts=echarts
